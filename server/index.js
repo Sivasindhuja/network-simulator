d4199ce7f678741841c66d3afb8bc8659baf74aa
Sivasindhuja <sindhutsundupalli@gmail.com>
2025-11-09 15:31:30 +0530

    Phase-1-- added logging middleware  to show the HTTP request

--- server/index.js
+++ server/index.js
@@ -2,8 +2,20 @@ import express from "express";
 
 const app=express();
 
+
+//middleware to log the request details
+
+function logging(req,res,next){
+    console.log(req.method);
+    console.log(req.originalUrl);
+    console.log(req.ip);
+     console.log(req.headers);
+    next();
+
+}
+app.use(logging);
 app.get("/",(req,res)=>{
-    console.log("Internet Mini simulator running....")
+    res.send("<h1>Welcome to the mini internet Simulator!!</h1>")
 })
 
 app.listen(3000,()=>{
